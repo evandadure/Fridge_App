dec5d2deb70adf5fa5000a80c69d10dad022e2ab
Evan Dadure <evan.dadure@gmail.com>
2018-04-29 21:51:04 +0300

    COMMIT 4

--- index.php
+++ index.php
@@ -4,8 +4,23 @@
 
 ?>
 
+    <div class="container">
 
-<h1>Welcome in Fridgee.</h1>
+      <div class="row row-offcanvas row-offcanvas-right">
+
+        <div class="col-12 col-md-8">
+          <p class="float-right hidden-md-up">
+            <button type="button" class="btn btn-primary btn-sm" data-toggle="offcanvas">Show my ingredients</button>
+          </p>
+          <div class="jumbotron" style="background-color: #74b9ff">
+            <h3>Not the courage to go down to the local grocery store buy frozen food?</h3>
+            <h3>Lack of inspiration to cook a small dish with the remains of the refrigerator?</h3>
+          </div>
+    <div class="container">
+<h1 style="color:red">Welcome in Fridgee.</h1>
+</div>
+
+This site will allow you to find in 3 clicks a cooking recipe with the remains of your fridge or your dusty closets. Sign in or sign up and enjoy our tools !
 
 <?php
   include_once('footer.php');
